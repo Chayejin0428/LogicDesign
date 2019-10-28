ade60ddf0dc1bba901d10e8b8340b3aca2263495
ChaYeJin1 <cha9599831@daum.net>
2019-10-29 04:15:00 +0900

    practice(except 5)

---
+++ practice01/practice01.v
@@ -0,0 +1,24 @@
+module carWarning;
+
+	input DoorClose		;
+	input Ignition		;
+	input SeatBelt		;
+
+	output Alarm		;
+
+	wire DoorOpened		;
+	wire NoSeatBelt		;
+
+	wire Node1		;
+	wire Node2		;
+
+	not NOT_U0(DoorOpened, DoorClose)		;
+	not NOT_U1(NoSeatBelt, SeatBelt)		;
+
+	and AND_U0(Node1, DoorOpened, Ignition)	;
+	and AND_U0(Node2, NoSeatBelt, Ignition)	;
+
+	or OR_U0(Alarm, Node1, Node2)		;
+
+endmodule
+
---
+++ practice04/tb_prj04_decoder.v
@@ -0,0 +1,49 @@
+module	tb_decoder;
+
+	wire	[7:0] out		;
+
+	reg	[2:0] in		;
+	reg	en		;
+
+//-----------------------------------------------------
+// Instances
+//-----------------------------------------------------
+
+	dec3to8_shift dut0(	.out	(out1	),
+			.in	(in	), 
+			.en	(en	));
+			
+	dec3to8_case dut1(	.out	(out2	),
+			.in	(in	), 
+			.en	(en	));
+
+//-----------------------------------------------------
+// Stimulus
+//-----------------------------------------------------
+
+	initial begin
+       	$display("dec3to8_shift : out1");
+	$display("dec3to8_case : out2");
+	$display("=============================================================================================");
+	$display("in[0]	in[1]	in[2]	en	out1	out2");
+	$display("=============================================================================================");
+	#(50)	{in[0], in[1], in[2], en} = 4'b0000;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	{in[0], in[1], in[2], en} = 4'b0001;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	{in[0], in[1], in[2], en} = 4'b0010;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	{in[0], in[1], in[2], en} = 4'b0011;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	{in[0], in[1], in[2], en} = 4'b0100;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	{in[0], in[1], in[2], en} = 4'b0101;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	{in[0], in[1], in[2], en} = 4'b0110;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	{in[0], in[1], in[2], en} = 4'b0111;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+       	#(50)	{in[0], in[1], in[2], en} = 4'b1000;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+       	#(50)	{in[0], in[1], in[2], en} = 4'b1001;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+        	#(50)	{in[0], in[1], in[2], en} = 4'b1010;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+        	#(50)	{in[0], in[1], in[2], en} = 4'b1011;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+        	#(50)	{in[0], in[1], in[2], en} = 4'b1100;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+       	#(50)	{in[0], in[1], in[2], en} = 4'b1101;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+        	#(50)	{in[0], in[1], in[2], en} = 4'b1110;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+        	#(50)	{in[0], in[1], in[2], en} = 4'b1111;	#(50)	$display("%b\t%b\t%b\t%b\t%b\t%b", in[0], in[1], in[2], en, out1, out2);
+	#(50)	$finish;
+end
+endmodule
+
